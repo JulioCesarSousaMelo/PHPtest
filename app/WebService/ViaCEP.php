f4c7286d996fd30a900b4262e0112380f5a31224
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-09-12 16:39:29 -0300

    ==app + metodo para consultar CEP na classe ViaCEP.php

---
+++ app/WebService/ViaCEP.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\WebService;
+
+class ViaCEP{
+
+    /** 
+     * Método responsável por consultar um CEP na API da ViaCEP
+     * @param string $cep
+     * @return array 
+    */
+    public static function consultarCEP($cep){
+
+        //INICIAR O CURL 
+        $curl = curl_init();
+
+        //CONFIGURAÇÕES DO CURL
+        //ARRAY DE CONFIGURAÇÕES
+        curl_setopt_array($curl,[
+            CURLOPT_URL => 'viacep.com.br/ws/'.$cep.'/xml/',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'GET'
+        ]);
+
+        //EXECUTAR
+        $response = curl_exec($curl);
+
+        //FECHAR A CONEXÃO
+        curl_close($curl);
+
+        //CONVERTER O XML PARA ARRAY
+        $array = simplexml_load_string($response);
+
+        //RETORNAR O CONTEÚDO EM ARRAY
+        return isset($array->cep) ? $array : null;
+          
+    }
+}
