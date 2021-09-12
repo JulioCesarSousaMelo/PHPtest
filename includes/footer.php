2a0142b6af2e65daa40b91e613b1ddbe57b704d2
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-09-12 18:47:47 -0300

    ==includes + adicao de tres arquivos

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    <!-- container -->
+    </div>
+    <footer class="bg-dark text-light">
+        <p>Exemplo de CRUD</p>
+    </footer>
+  </body>
+</html>
---
+++ includes/formulario.php
@@ -0,0 +1,8 @@
+<form class="row align-items-center" action="" method="POST"> 
+  <div class="col-9">
+    <input type="text" class="form-control form-control-lg" placeholder="Informe o CEP desejado" require>
+  </div>
+  <div class="col-3">
+    <button type="submit" class="btn btn-success btn-lg">Pesquisar</button>
+  </div>
+</form>
