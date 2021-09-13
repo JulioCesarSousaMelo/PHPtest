4190a0aa714c9ebc969c0c3dc30fbfb91758d4a8
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-09-13 03:01:03 -0300

    ==app\Entity + adicao de metodos em Cep.php

--- app/Entity/Cep.php
+++ app/Entity/Cep.php
@@ -2,44 +2,67 @@
 
 namespace App\Entity;
 
+use \App\Db\Database;
+
 class Cep{
     
-    /**
-     * Identificador único do cep
-     * @var int
-     */
     public $id;
-
-    /**
-     * Número do cep
-     * @var string
-     */
     public $cep;
-
-    /**
-     * Logradouro do cep
-     * @var string
-     */
     public $logradouro;
-
-    /**
-     * Bairro do cep
-     * @var string
-     */
     public $bairro;
-
-    /**
-     * Localidade do cep
-     * @var string
-     */
     public $localidade;
-
-    /**
-     * UF do cep
-     * @var string
-     */
     public $uf;
 
     
 
+    /**
+     * Método responsável por verificar se o cep já existe no banco
+     * @param string $cep_form
+     * @return boolean
+     */
+    public function verificarCEP($cep_form){
+        $this->cep = $cep_form;
+
+        $obj_database = new Database('cep');
+        $array = $obj_database->select($this->cep);
+
+        if($array[cep]){
+            return true;
+        }
+    }
+
+    /**
+     * Método responsável por armazenar campos do banco em atributos da classe
+     * 
+     */
+    public function armazenarAtributos(){
+        $obj_database = new Database('cep');
+        $array = $obj_database->select($this->cep);
+
+        $this->logradouro = $array['logradouro'];
+        $this->bairro = $array['bairro'];
+        $this->localidade = $array['localidade'];
+        $this->uf = $array['uf'];
+    }
+
+    /**
+     * Método responsável por exibir os atributos do cep
+     * 
+     */
+    public function exibirCEP(){
+        echo $this->cep;
+        echo $this->logradouro;
+        echo $this->bairro;
+        echo $this->localidade;
+        echo $this->uf;
+    }
+
+     /**
+     * Método responsável por inserir um cep no banco
+     * @param string $values
+     */
+    public function inserirCEP($values){
+        $obj_database = new Database('cep');
+        $obj_database->insert($values);
+    }
 }
