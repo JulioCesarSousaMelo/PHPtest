dac9cecc6d5738f5ce3f3fb626a5e037cbf207c6
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-09-13 04:09:52 -0300

    ==includes + adicao de script em Header.php + adicao de mascara no input cep em Formulario.php

--- includes/formulario.php
+++ includes/formulario.php
@@ -1,6 +1,6 @@
 <form class="row align-items-center" method="POST"> 
   <div class="col-9">
-    <input type="text" name="cep" class="form-control form-control-lg" placeholder="Informe o CEP desejado" required>
+    <input type="text" name="cep" id="cep" class="form-control form-control-lg" placeholder="Informe o CEP desejado" maxlength="9" onkeyup="mascara_cep()" required>
   </div>
   <div class="col-3">
     <button type="submit" class="btn btn-success btn-lg">Pesquisar</button>
